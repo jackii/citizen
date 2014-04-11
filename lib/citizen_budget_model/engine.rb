0e5ec1d5c7a693da679cb680575c6ea91d842348
James McKinney <james@slashpoundbang.com>
2014-04-11 17:11:17 -0400

    add test framework

--- lib/citizen_budget_model/engine.rb
+++ lib/citizen_budget_model/engine.rb
@@ -1,5 +1,10 @@
 module CitizenBudgetModel
   class Engine < ::Rails::Engine
     isolate_namespace CitizenBudgetModel
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
