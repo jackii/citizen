aa8949d6aa971739c41b5830aefc5161709759a5
James McKinney <james@slashpoundbang.com>
2014-08-17 15:49:53 -0400

    relate organizations and users

--- app/models/citizen_budget_model/organization.rb
+++ app/models/citizen_budget_model/organization.rb
@@ -4,6 +4,7 @@ module CitizenBudgetModel
     translates :name
 
     has_many :simulators, dependent: :destroy
+    has_many :users
 
     validates_presence_of :name
   end
--- app/models/citizen_budget_model/user.rb
+++ app/models/citizen_budget_model/user.rb
@@ -2,5 +2,7 @@ module CitizenBudgetModel
   class User < ActiveRecord::Base
     devise :database_authenticatable,
            :recoverable, :rememberable, :trackable, :validatable
+
+    belongs_to :organization
   end
 end
