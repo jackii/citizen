7f185ec0011dea89aded3a6041a78d92bb5e91f2
James McKinney <james@slashpoundbang.com>
2014-08-17 17:23:55 -0400

    fix path to po file

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,5 +1,5 @@
 # @see https://github.com/grosser/fast_gettext#2-add-a-translation-repository
 # @see https://github.com/grosser/gettext_i18n_rails#locales--initialisation
-FastGettext.add_text_domain('citizen_budget_model', path: 'po', type: :po) # same path as gettext gem
+FastGettext.add_text_domain('citizen_budget_model', path: File.expand_path('../../po', __dir__), type: :po) # same path as gettext gem
 FastGettext.default_text_domain = 'citizen_budget_model'
 FastGettext.default_available_locales = ['en-CA', 'fr-CA']
