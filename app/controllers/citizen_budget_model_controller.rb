84fd0f19ee7658f906f3e7e01378340fc7383b12
James McKinney <james@slashpoundbang.com>
2014-08-17 15:46:07 -0400

    rename before_filter and locales

--- app/controllers/citizen_budget_model_controller.rb
+++ app/controllers/citizen_budget_model_controller.rb
@@ -1,9 +1,10 @@
+# @see https://github.com/grosser/fast_gettext#rails
 class CitizenBudgetModelController < CitizenBudgetModel.parent_controller.constantize
   include FastGettext::Translation
 
-  before_filter :set_gettext_locale
+  before_filter :set_locale
 
-  def set_gettext_locale
-    session[:locale] = FastGettext.set_locale(params[:locale] || session[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE'] || 'en_CA')
+  def set_locale
+    session[:locale] = I18n.locale = FastGettext.set_locale(params[:locale] || session[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE'] || 'en-CA')
   end
 end
--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,6 +1,5 @@
-# Uses the same path as the gettext gem.
 # @see https://github.com/grosser/fast_gettext#2-add-a-translation-repository
 # @see https://github.com/grosser/gettext_i18n_rails#locales--initialisation
-FastGettext.add_text_domain('citizen_budget_model', path: 'po', type: :po)
+FastGettext.add_text_domain('citizen_budget_model', path: 'po', type: :po) # same path as gettext gem
 FastGettext.default_text_domain = 'citizen_budget_model'
-FastGettext.default_available_locales = ['en_CA', 'fr_CA']
+FastGettext.default_available_locales = ['en-CA', 'fr-CA']
