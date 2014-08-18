0de56a4eadf4c78a9167963e000404ce0defd518
James McKinney <james@slashpoundbang.com>
2014-08-17 23:33:39 -0400

    switch back to po files for now

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,7 +1,9 @@
 # @see https://github.com/grosser/fast_gettext#2-add-a-translation-repository
 # @see https://github.com/grosser/gettext_i18n_rails#locales--initialisation
-require 'fast_gettext/translation_repository/db'
-FastGettext::TranslationRepository::Db.require_models
-FastGettext.add_text_domain('citizen_budget_model', type: :db)
+# @todo Using the DB causes test failures.
+# require 'fast_gettext/translation_repository/db'
+# FastGettext::TranslationRepository::Db.require_models
+# FastGettext.add_text_domain('citizen_budget_model', type: :db)
+FastGettext.add_text_domain('citizen_budget_model', path: File.expand_path('../../po', __dir__), type: :po) # same path as gettext gem
 FastGettext.default_text_domain = 'citizen_budget_model'
 FastGettext.default_available_locales = ['en-CA', 'fr-CA']
