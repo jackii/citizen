5c6cacbd6b3ed34c3fc6d61f6c24b80f813429e6
James McKinney <james@slashpoundbang.com>
2014-08-18 19:18:01 -0400

    add layout for devise controllers

---
+++ app/controllers/citizen_budget_devise_controller.rb
@@ -0,0 +1,12 @@
+class CitizenBudgetDeviseController < CitizenBudgetModel.parent_controller.constantize
+  include FastGettext::Translation
+  helper_method :_
+
+  layout 'citizen_budget_model/devise'
+
+  before_filter :set_locale
+
+  def set_locale
+    session[:locale] = FastGettext.set_locale(params[:locale] || session[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE'] || 'en_CA')
+  end
+end
