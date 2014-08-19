538a96fe9a4321b364a23a76e02634c10cc53e3a
James McKinney <james@slashpoundbang.com>
2014-08-18 22:32:58 -0400

    get pages controller specs passing

--- spec/controllers/citizen_budget_model/pages_controller_spec.rb
+++ spec/controllers/citizen_budget_model/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 module CitizenBudgetModel
   describe PagesController do
+    routes { CitizenBudgetModel::Engine.routes }
+
     describe 'GET home' do
       it 'returns http success' do
         get 'home'
