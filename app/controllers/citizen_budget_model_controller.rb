78d0c8457f2f87544ffc321eb432d76762918274
James McKinney <james@slashpoundbang.com>
2014-08-19 19:52:02 -0400

    implement simulator views, start a form builder

--- app/controllers/citizen_budget_model_controller.rb
+++ app/controllers/citizen_budget_model_controller.rb
@@ -12,3 +12,57 @@ class CitizenBudgetModelController < CitizenBudgetModel.parent_controller.consta
   end
   helper_method :admin?
 end
+
+# There's no convention for where to put form builders such that they reload in development.
+module CitizenBudgetModel
+  class CitizenBudgetFormBuilder < ActionView::Helpers::FormBuilder
+    def errors
+      if object.errors.any?
+        content = ActiveSupport::SafeBuffer.new
+        object.errors.full_messages.each do |message|
+          content << @template.content_tag(:li, message)
+        end
+        @template.content_tag(:div, @template.content_tag(:ul, content), class: 'alert alert-danger')
+      end
+    end
+
+    def translated_text_field(method, options = {}) # @todo continue
+      if object.class.translated_attribute_names.include?(method)
+        object.class.globalize_locales.each do |locale|
+          @template.concat(text_field(method, options, locale))
+        end
+        ''
+      else
+        text_field(method, options)
+      end
+    end
+
+    def text_field(method, options = {}, locale = nil)
+      field_method = method
+      if locale
+        field_method = object.class.localized_attr_name_for(method, locale)
+      end
+      klass = 'form-group'
+      content = label(method) + super(field_method, {class: 'form-control'}.merge(options))
+      if object.errors[method].any?
+        klass << ' has-error has-feedback'
+        content << @template.content_tag(:span, nil, class: 'glyphicon glyphicon-remove form-control-feedback')
+      end
+      @template.content_tag(:div, content, class: klass)
+    end
+
+    def select(method, choices = nil, options = {}, html_options = {})
+      klass = 'form-group'
+      content = label(method) + super(method, choices, options, {class: 'form-control'}.merge(html_options))
+      if object.errors[method].any?
+        klass << ' has-error has-feedback'
+        content << @template.content_tag(:span, nil, class: 'glyphicon glyphicon-remove form-control-feedback')
+      end
+      @template.content_tag(:div, content, class: klass)
+    end
+
+    def save
+      button _('Save'), class: 'btn btn-primary'
+    end
+  end
+end
