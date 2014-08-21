91fc6617b2fe79ec19f0486dd100fd4c0adf2d75
James McKinney <james@slashpoundbang.com>
2014-08-21 18:30:44 -0400

    implement show view for questions and sections

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -2,4 +2,9 @@ ActiveRecord::Base.class_eval do
   def in_any_locale?(attribute)
     I18n.available_locales.any?{|locale| send(self.class.localized_attr_name_for(attribute, locale)).present?}
   end
+
+  def any_of?(*attributes)
+    translated, untranslated = attributes.partition{|attribute| self.class.translated_attribute_names.include?(attributes)}
+    translated.any?{|attribute| in_any_locale?(attribute)} || untranslated.any?{|attribute| self[attribute].present?}
+  end
 end
