ede904e4495651226012ddaacf34a9d4580c8830
James McKinney <james@slashpoundbang.com>
2014-08-21 17:17:22 -0400

    add method to activerecord

--- app/validators/citizen_budget_model/locale_validator.rb
+++ app/validators/citizen_budget_model/locale_validator.rb
@@ -2,7 +2,7 @@ module CitizenBudgetModel
   # @see https://github.com/globalize/globalize-accessors/issues/8
   class LocaleValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      if I18n.available_locales.all?{|locale| record.send(record.class.localized_attr_name_for(attribute, locale)).blank?}
+      unless record.in_any_locale?(attribute)
         record.errors.add(attribute, :blank)
       end
     end
