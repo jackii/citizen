a2e77dc3702d25575005d7ed0f42f137699e450f
James McKinney <james@slashpoundbang.com>
2014-08-22 18:29:23 -0400

    redirect to simulators after sign in

--- app/controllers/citizen_budget_model/devise_controller.rb
+++ app/controllers/citizen_budget_model/devise_controller.rb
@@ -1,5 +1,9 @@
 module CitizenBudgetModel
   class DeviseController < CitizenBudgetModelController
     layout 'citizen_budget_model/devise'
+
+    def after_sign_in_path_for(resource)
+      simulators_path
+    end
   end
 end
