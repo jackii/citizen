56b37ec5e30ba02205cd020a940c6706a36595c1
James McKinney <james@slashpoundbang.com>
2014-10-10 15:54:08 -0400

    fix some js warnings

--- app/assets/javascripts/citizen_budget_model/main.js
+++ app/assets/javascripts/citizen_budget_model/main.js
@@ -1,17 +1,18 @@
+/* jshint laxcomma: true */
 jQuery(function ($) {
   $('.sortable').sortable({
     axis: 'y'
   , cursor: 'move'
   , handle: 'span.glyphicon-resize-vertical'
-  , update: function (event, ui) {
-      $target = $(event.target);
+  , update: function (event) {
+      var $target = $(event.target);
       $.ajax({
         type: 'POST'
       , url: location.href.split('?')[0].split('#')[0] + '/sort'
       , data: $target.sortable('serialize')
-      }).done(function (request) {
+      }).done(function () {
         $target.effect('highlight');
-      })
+      });
     }
   });
 
