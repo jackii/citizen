266a33c1bae6418bfab7ee5a5c38722dcaf476b5
James McKinney <james@slashpoundbang.com>
2014-10-22 01:15:14 -0400

    allow total revenue in billions

--- db/migrate/20141022050318_add_total_revenue_to_citizen_budget_model_simulator.rb
+++ db/migrate/20141022050318_add_total_revenue_to_citizen_budget_model_simulator.rb
@@ -1,5 +1,5 @@
 class AddTotalRevenueToCitizenBudgetModelSimulator < ActiveRecord::Migration
   def change
-    add_column :citizen_budget_model_simulators, :total_revenue, :integer
+    add_column :citizen_budget_model_simulators, :total_revenue, :integer, limit: 8
   end
 end
